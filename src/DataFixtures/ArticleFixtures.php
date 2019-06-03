d3c24f7114482ad62fae9b5d686af80d572843e6
Quentin LATOUR <Quentour69100@gmail.com>
2019-06-03 10:27:13 +0200

    faker

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use  Faker;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+
+{
+
+    public function load(ObjectManager $manager)
+    {
+        
+        $faker  =  Faker\Factory::create('fr_FR');
+        for($i=0; $i<50; $i++)
+        {
+
+            $article = new Article();
+            $article->setTitle(mb_strtolower($faker->sentence()));
+            $article->setContent($faker->sentence());
+        if ($i<10)
+        {
+            $article->setCategory($this->getReference('categorie_0'));      
+        }
+        else if ($i<20)
+        {
+            $article->setCategory($this->getReference('categorie_1'));      
+        }
+        else if ($i<30)
+        {
+            $article->setCategory($this->getReference('categorie_2'));      
+        }
+        else if ($i<40)
+        {
+            $article->setCategory($this->getReference('categorie_3'));      
+        }
+        else if ($i<50)
+        {
+            $article->setCategory($this->getReference('categorie_4'));      
+        }
+            $manager->persist($article);
+            $manager->flush();
+            
+        }
+    }
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+}
